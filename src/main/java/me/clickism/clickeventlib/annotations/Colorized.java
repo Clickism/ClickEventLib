c399463d1c19c429d7a9310b50f1fe533f0ded50
Clickism <yagizaktasx@gmail.com>
2024-12-22 07:15:28 +0100

    add colorized annotation

---
+++ src/main/java/me/clickism/clickeventlib/annotations/Colorized.java
@@ -0,0 +1,12 @@
+package me.clickism.clickeventlib.annotations;
+
+import me.clickism.clickeventlib.util.Utils;
+import java.lang.annotation.Documented;
+
+/**
+ * Annotation to mark string arguments that are automatically colorized.
+ * See {@link Utils#colorize(String)}.
+ */
+@Documented
+public @interface Colorized {
+}
