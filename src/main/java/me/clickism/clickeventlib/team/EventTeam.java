69dd882b4777ea67de0a9a6b59f017d7c0e1c253
Clickism <yagizaktasx@gmail.com>
2025-01-28 23:24:03 +0100

    refactor to use team manager singleton

--- src/main/java/me/clickism/clickeventlib/team/EventTeam.java
+++ src/main/java/me/clickism/clickeventlib/team/EventTeam.java
@@ -3,6 +3,9 @@ package me.clickism.clickeventlib.team;
 import me.clickism.clickeventlib.util.Identifier;
 import me.clickism.subcommandapi.util.Named;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+
+import java.util.Objects;
 
 /**
  * Represents a team in an event.
@@ -14,10 +17,6 @@ public class EventTeam implements Named {
      */
     public enum FriendlyFireSetting {
         /**
-         * Use the default setting.
-         */
-        USE_DEFAULT,
-        /**
          * Friendly fire is allowed.
          */
         ALLOWED,
@@ -27,165 +26,180 @@ public class EventTeam implements Named {
         NOT_ALLOWED
     }
 
-    private static final String DEFAULT_PREFIX = "[👥] " + ChatColor.RESET;
+    /**
+     * Default prefix for teams.
+     */
+    public static final String DEFAULT_PREFIX = "[👥] " + ChatColor.RESET;
 
     /**
      * Red team.
      */
-    public static final EventTeam RED = new EventTeam("red", ChatColor.RED);
+    public static final EventTeam RED = new EventTeam("red").setColor(ChatColor.RED);
     /**
      * Blue team.
      */
-    public static final EventTeam BLUE = new EventTeam("blue", ChatColor.BLUE);
+    public static final EventTeam BLUE = new EventTeam("blue").setColor(ChatColor.BLUE);
     /**
      * Green team.
      */
-    public static final EventTeam GREEN = new EventTeam("green", ChatColor.GREEN);
+    public static final EventTeam GREEN = new EventTeam("green").setColor(ChatColor.GREEN);
     /**
      * Yellow team.
      */
-    public static final EventTeam YELLOW = new EventTeam("yellow", ChatColor.YELLOW);
+    public static final EventTeam YELLOW = new EventTeam("yellow").setColor(ChatColor.YELLOW);
     /**
      * Orange team.
      */
-    public static final EventTeam ORANGE = new EventTeam("orange", ChatColor.GOLD);
+    public static final EventTeam ORANGE = new EventTeam("orange").setColor(ChatColor.GOLD);
     /**
      * Anthracite team.
      */
-    public static final EventTeam ANTHRACITE = new EventTeam("anthracite", ChatColor.DARK_GRAY);
+    public static final EventTeam ANTHRACITE = new EventTeam("anthracite").setColor(ChatColor.DARK_GRAY);
     /**
      * Gray team.
      */
-    public static final EventTeam GRAY = new EventTeam("gray", ChatColor.GRAY);
+    public static final EventTeam GRAY = new EventTeam("gray").setColor(ChatColor.GRAY);
     /**
      * Cyan team.
      */
-    public static final EventTeam CYAN = new EventTeam("cyan", ChatColor.AQUA);
+    public static final EventTeam CYAN = new EventTeam("cyan").setColor(ChatColor.AQUA);
     /**
      * Moss team.
      */
-    public static final EventTeam MOSS = new EventTeam("moss", ChatColor.DARK_GREEN);
+    public static final EventTeam MOSS = new EventTeam("moss").setColor(ChatColor.DARK_GREEN);
     /**
      * Purple team.
      */
-    public static final EventTeam PURPLE = new EventTeam("purple", ChatColor.DARK_PURPLE);
+    public static final EventTeam PURPLE = new EventTeam("purple").setColor(ChatColor.DARK_PURPLE);
     /**
      * Blood team.
      */
-    public static final EventTeam BLOOD = new EventTeam("blood", ChatColor.DARK_RED);
+    public static final EventTeam BLOOD = new EventTeam("blood").setColor(ChatColor.DARK_RED);
     /**
      * Ocean team.
      */
-    public static final EventTeam OCEAN = new EventTeam("ocean", ChatColor.DARK_AQUA);
+    public static final EventTeam OCEAN = new EventTeam("ocean").setColor(ChatColor.DARK_AQUA);
 
     private final String name;
-    private final ChatColor color;
-    private final String prefix;
+    private ChatColor color = ChatColor.RESET;
+    private String prefix = DEFAULT_PREFIX;
 
-    private final JoinSetting joinSetting;
-
-    private final FriendlyFireSetting friendlyFireSetting;
-
-    /**
-     * Creates a new team with the given id and color.
-     * Uses {@link JoinSetting#USE_DEFAULT} and {@link FriendlyFireSetting#USE_DEFAULT}.
-     *
-     * @param id    the id of the team
-     * @param color the color of the team
-     */
-    public EventTeam(Identifier id, ChatColor color) {
-        this(id, color, DEFAULT_PREFIX, JoinSetting.USE_DEFAULT, FriendlyFireSetting.USE_DEFAULT);
-    }
+    private JoinSetting joinSettingOverride = null;
+    private Boolean friendlyFireOverride = null;
 
     /**
-     * Creates a new team with the given name and color.
-     * Uses {@link JoinSetting#USE_DEFAULT} and {@link FriendlyFireSetting#USE_DEFAULT}.
+     * Creates a new team with the given name and registers it with the team manager.
      *
-     * @param name  the name of the team
-     * @param color the color of the team
+     * @param name the name of the team
      */
-    public EventTeam(String name, ChatColor color) {
-        this(name, color, DEFAULT_PREFIX, JoinSetting.USE_DEFAULT, FriendlyFireSetting.USE_DEFAULT);
-    }
-
-    /**
-     * Creates a new team with the given id, color, team setting, and friendly fire setting.
-     *
-     * @param id                  the id of the team
-     * @param color               the color of the team
-     * @param joinSetting         the team setting of the team
-     * @param friendlyFireSetting whether friendly fire is allowed
-     */
-    public EventTeam(Identifier id, ChatColor color, JoinSetting joinSetting, FriendlyFireSetting friendlyFireSetting) {
-        this(id, color, DEFAULT_PREFIX, joinSetting, friendlyFireSetting);
-    }
-
-    /**
-     * Creates a new team with the given name, color, team setting, and friendly fire setting.
-     *
-     * @param name                the name of the team
-     * @param color               the color of the team
-     * @param joinSetting         the team setting of the team
-     * @param friendlyFireSetting whether friendly fire is allowed
-     */
-    public EventTeam(String name, ChatColor color, JoinSetting joinSetting, FriendlyFireSetting friendlyFireSetting) {
-        this(name, color, DEFAULT_PREFIX, joinSetting, friendlyFireSetting);
-    }
-
-    /**
-     * Creates a new team with the given id, color, prefix, team setting, and friendly fire setting.
-     *
-     * @param id                  the id of the team
-     * @param color               the color of the team
-     * @param prefix              the prefix of the team
-     * @param joinSetting         the team setting of the team
-     * @param friendlyFireSetting whether friendly fire is allowed
-     */
-    public EventTeam(Identifier id, ChatColor color, String prefix, JoinSetting joinSetting, FriendlyFireSetting friendlyFireSetting) {
-        this(id.toString(), color, prefix, joinSetting, friendlyFireSetting);
-    }
-
-    /**
-     * Creates a new team with the given name, color, prefix, team setting, and friendly fire setting.
-     *
-     * @param name                the name of the team
-     * @param color               the color of the team
-     * @param prefix              the prefix of the team
-     * @param joinSetting         the team setting of the team
-     * @param friendlyFireSetting whether friendly fire is allowed
-     */
-    public EventTeam(String name, ChatColor color, String prefix, JoinSetting joinSetting, FriendlyFireSetting friendlyFireSetting) {
+    protected EventTeam(String name) {
         this.name = name;
+        TeamManager.INSTANCE.registerTeam(this);
+    }
+
+    /**
+     * Joins a player to this team.
+     *
+     * @param player the player to join
+     */
+    public void joinTeam(OfflinePlayer player) {
+        joinTeam(player.getName());
+    }
+
+    /**
+     * Joins an entry to this team.
+     *
+     * @param entry the entry to join
+     */
+    public void joinTeam(String entry) {
+        TeamManager.INSTANCE.joinTeam(entry, this);
+    }
+
+    /**
+     * Leaves a player from this team.
+     *
+     * @param player the player to leave
+     */
+    public void leaveTeam(OfflinePlayer player) {
+        leaveTeam(player.getName());
+    }
+
+    /**
+     * Leaves an entry from this team.
+     *
+     * @param entry the entry to leave
+     */
+    public void leaveTeam(String entry) {
+        TeamManager.INSTANCE.leaveTeam(entry);
+    }
+
+    /**
+     * Checks if a player is on this team.
+     *
+     * @param player the player to check
+     * @return true if the player is on the team, false otherwise
+     */
+    public boolean isOnTeam(OfflinePlayer player) {
+        return isOnTeam(player.getName());
+    }
+
+    /**
+     * Checks if an entry is on this team.
+     *
+     * @param entry the entry to check
+     * @return true if the entry is on the team, false otherwise
+     */
+    public boolean isOnTeam(String entry) {
+        return TeamManager.INSTANCE.isOnTeam(entry, this);
+    }
+
+    /**
+     * Sets the color of the team.
+     *
+     * @param color the color of the team
+     * @return this team
+     */
+    public EventTeam setColor(ChatColor color) {
         this.color = color;
-        this.prefix = color + prefix;
-        this.joinSetting = joinSetting;
-        this.friendlyFireSetting = friendlyFireSetting;
+        return this;
     }
 
     /**
-     * Gets the team setting for this team.
+     * Sets the prefix of this team.
      *
-     * @param defaultJoinSetting the default team setting to use if this team uses {@link JoinSetting#USE_DEFAULT}
-     * @return the team setting for this team
+     * @param prefix the prefix of this team
+     * @return this team
      */
-    public JoinSetting getJoinSetting(JoinSetting defaultJoinSetting) {
-        if (joinSetting == JoinSetting.USE_DEFAULT) {
-            return defaultJoinSetting;
-        }
-        return joinSetting;
+    public EventTeam setPrefix(String prefix) {
+        this.prefix = prefix;
+        return this;
     }
 
     /**
-     * Returns whether friendly fire is allowed for this team.
+     * Sets the join setting override for this team.
      *
-     * @param defaultAllowFriendlyFire the default friendly fire setting to use if this team uses {@link FriendlyFireSetting#USE_DEFAULT}
-     * @return whether friendly fire is allowed for this team
+     * @param joinSetting the join setting for this team
+     * @return this team
      */
-    public boolean isFriendlyFireAllowed(boolean defaultAllowFriendlyFire) {
-        if (friendlyFireSetting == FriendlyFireSetting.USE_DEFAULT) {
-            return defaultAllowFriendlyFire;
-        }
-        return friendlyFireSetting == FriendlyFireSetting.ALLOWED;
+    public EventTeam setJoinSettingOverride(JoinSetting joinSetting) {
+        this.joinSettingOverride = joinSetting;
+        return this;
+    }
+
+    /**
+     * Sets the friendly fire setting override for this team.
+     *
+     * @param allowFriendlyFire the friendly fire setting for this team
+     * @return this team
+     */
+    public EventTeam setFriendlyFireOverride(boolean allowFriendlyFire) {
+        this.friendlyFireOverride = allowFriendlyFire;
+        return this;
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 
     /**
@@ -206,9 +220,38 @@ public class EventTeam implements Named {
         return prefix;
     }
 
-    @Override
-    public String getName() {
-        return name;
+    /**
+     * Gets the join setting for this team or the default join setting if no override is set.
+     *
+     * @return the join setting for this team
+     */
+    public JoinSetting getJoinSetting() {
+        return Objects.requireNonNullElseGet(
+                joinSettingOverride,
+                TeamManager.INSTANCE::getDefaultJoinSetting
+        );
+    }
+
+    /**
+     * Gets the friendly fire setting for this team or the default friendly fire setting if no override is set.
+     *
+     * @return the friendly fire for this team
+     */
+    public boolean isFriendlyFireAllowed() {
+        return Objects.requireNonNullElseGet(
+                friendlyFireOverride,
+                TeamManager.INSTANCE::getDefaultAllowFriendlyFire
+        );
+    }
+
+    /**
+     * Creates a new team with the given name and registers it with the team manager.
+     *
+     * @param identifier the identifier of the team
+     * @return the new team
+     */
+    public static EventTeam of(Identifier identifier) {
+        return new EventTeam(identifier.toString());
     }
 
     /**
