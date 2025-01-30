96745c7a0a136725cc6d54262c22a4300ceba842
Clickism <yagizaktasx@gmail.com>
2025-01-30 21:32:59 +0100

    register PhaseStartSubcommand

--- src/main/java/me/clickism/clickeventlib/commands/phase/PhaseSubcommandGroup.java
+++ src/main/java/me/clickism/clickeventlib/commands/phase/PhaseSubcommandGroup.java
@@ -14,6 +14,7 @@ public class PhaseSubcommandGroup extends SubcommandGroup {
      */
     public PhaseSubcommandGroup(PhaseManager phaseManager) {
         super("phase", true);
+        addSubcommand(new PhaseStartSubcommand(phaseManager));
         addSubcommand(new PhaseSetSubcommand(phaseManager));
         addSubcommand(new PhaseTimerSubcommand(phaseManager));
         addSubcommand(new PhaseSkipSubcommand(phaseManager));
